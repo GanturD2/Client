f8bed3a011f4925dd6620f5bfcc0bba177971d03
GanturD2 <105028658+GanturD2@users.noreply.github.com>
2025-01-25 06:38:11 +0100

    Add files via upload

---
+++ PRTerrainLib/Terrain.cpp
@@ -0,0 +1,355 @@
+#include "StdAfx.h"
+#include "../EterPack/EterPackManager.h"
+
+#include "Terrain.h"
+#include <cmath>
+
+//////////////////////////////////////////////////////////////////////////
+// Texture Set
+//////////////////////////////////////////////////////////////////////////
+CTextureSet * CTerrainImpl::ms_pTextureSet = nullptr;
+CTextureSet* CTerrainImpl::ms_pSnowTextureSet = nullptr;
+
+void CTerrainImpl::SetTextureSet(CTextureSet * pTextureSet)
+{
+	static CTextureSet s_EmptyTextureSet;
+
+	if (!pTextureSet)
+		ms_pTextureSet = &s_EmptyTextureSet;
+	else
+		ms_pTextureSet = pTextureSet;
+}
+
+CTextureSet * CTerrainImpl::GetTextureSet()
+{
+	if (!ms_pTextureSet)
+		SetTextureSet(nullptr);
+
+	return ms_pTextureSet;
+}
+
+void CTerrainImpl::SetSnowTextureSet(CTextureSet * pTextureSet)
+{
+	static CTextureSet s_EmptyTextureSet;
+
+	if (!pTextureSet)
+		ms_pSnowTextureSet = &s_EmptyTextureSet;
+	else
+		ms_pSnowTextureSet = pTextureSet;
+}
+
+CTextureSet * CTerrainImpl::GetSnowTextureSet()
+{
+	if (!ms_pSnowTextureSet)
+		SetSnowTextureSet(nullptr);
+
+	return ms_pSnowTextureSet;
+}
+
+CTerrainImpl::CTerrainImpl()
+{
+	Initialize();
+}
+
+CTerrainImpl::~CTerrainImpl()
+{
+	Clear();
+}
+
+void CTerrainImpl::Initialize()
+{
+	msl::refill(m_lWaterHeight, -1);
+
+	m_byNumWater = 0;
+	m_HeightMapHeader = {};
+	msl::refill(m_awShadowMap, 0xFFFF);
+	msl::refill(m_lpAlphaTexture);
+
+	m_lViewRadius = 0;
+
+	m_wTileMapVersion = 8976;
+
+	m_fHeightScale = 0.0f;
+
+	m_lpShadowTexture = nullptr;
+
+	m_lSplatTilesX = 0;
+	m_lSplatTilesY = 0;
+}
+
+void CTerrainImpl::Clear()
+{
+	for (uint32_t i = 0; i < GetTextureSet()->GetTextureCount(); ++i)
+	{
+		if (m_lpAlphaTexture[i])
+		{
+			m_lpAlphaTexture[i]->Release();
+			m_lpAlphaTexture[i] = nullptr;
+		}
+	}
+
+	Initialize();
+}
+
+bool CTerrainImpl::LoadHeightMap(const char * c_szFileName)
+{
+	Tracef("LoadRawHeightMapFile %s ", c_szFileName);
+
+	CMappedFile kMappedFile;
+	LPCVOID lpcvFileData;
+
+	if (!CEterPackManager::Instance().Get(kMappedFile, c_szFileName, &lpcvFileData))
+	{
+		Tracen("Error");
+		TraceError("CTerrainImpl::LoadHeightMap - %s OPEN ERROR", c_szFileName);
+		return false;
+	}
+
+	memcpy(m_awRawHeightMap, lpcvFileData, sizeof(uint16_t) * HEIGHTMAP_RAW_XSIZE * HEIGHTMAP_RAW_YSIZE);
+
+	return true;
+}
+
+bool CTerrainImpl::LoadAttrMap(const char * c_szFileName)
+{
+	uint32_t dwStart = ELTimer_GetMSec();
+	Tracef("LoadAttrMapFile %s ", c_szFileName);
+
+	CMappedFile kMappedFile;
+	LPCVOID lpcvFileData;
+
+	if (!CEterPackManager::Instance().Get(kMappedFile, c_szFileName, &lpcvFileData))
+	{
+		TraceError("CTerrainImpl::LoadAttrMap - %s OPEN ERROR", c_szFileName);
+		return false;
+	}
+
+	uint32_t dwFileSize = kMappedFile.Size();
+	auto * abFileData = (uint8_t *) lpcvFileData;
+
+	// LoadAttrMap
+	{
+#pragma pack(push)
+#pragma pack(1)
+		struct SAttrMapHeader
+		{
+			uint16_t m_wMagic;
+			uint16_t m_wWidth;
+			uint16_t m_wHeight;
+		};
+#pragma pack(pop)
+
+		if (dwFileSize < sizeof(SAttrMapHeader))
+		{
+			TraceError(" CTerrainImpl::LoadAttrMap - %s FILE SIZE ERROR", c_szFileName);
+			return false;
+		}
+
+		SAttrMapHeader kAttrMapHeader;
+		memcpy(&kAttrMapHeader, abFileData, sizeof(kAttrMapHeader));
+
+		const uint16_t c_wAttrMapMagic = 2634;
+		if (c_wAttrMapMagic != kAttrMapHeader.m_wMagic)
+		{
+			TraceError("CTerrainImpl::LoadAttrMap - %s MAGIC NUMBER(%d!=MAGIC[%d]) ERROR", c_szFileName, kAttrMapHeader.m_wMagic,
+					   kAttrMapHeader.m_wMagic);
+			return false;
+		}
+
+		if (ATTRMAP_XSIZE != kAttrMapHeader.m_wWidth)
+		{
+			TraceError("CTerrainImpl::LoadAttrMap - kAttrMapHeader(%s).m_width(%d)!=ATTRMAP_XSIZE(%d)", c_szFileName,
+					   kAttrMapHeader.m_wWidth, ATTRMAP_XSIZE);
+			return false;
+		}
+
+		if (ATTRMAP_YSIZE != kAttrMapHeader.m_wHeight)
+		{
+			TraceError("CTerrainImpl::LoadAttrMap - kAttrMapHeader(%s).m_height(%d)!=ATTRMAP_YSIZE(%d)", c_szFileName,
+					   kAttrMapHeader.m_wHeight, ATTRMAP_YSIZE);
+			return false;
+		}
+
+		uint32_t dwFileRestSize = dwFileSize - sizeof(kAttrMapHeader);
+		uint32_t dwFileNeedSize = sizeof(m_abyAttrMap);
+		if (dwFileRestSize != dwFileNeedSize)
+		{
+			TraceError("CTerrainImpl::LoadAttrMap - %s FILE DATA SIZE(rest %d != need %d) ERROR", c_szFileName, dwFileRestSize,
+					   dwFileNeedSize);
+			return false;
+		}
+
+		uint8_t * abSrcAttrData = abFileData + sizeof(kAttrMapHeader);
+		memcpy(m_abyAttrMap, abSrcAttrData, sizeof(m_abyAttrMap));
+	}
+
+	Tracef("%d\n", ELTimer_GetMSec() - dwStart);
+	return true;
+}
+
+bool CTerrainImpl::RAW_LoadTileMap(const char * c_szFileName)
+{
+	Tracef("LoadSplatFile %s ", c_szFileName);
+
+	CMappedFile kMappedFile;
+	LPCVOID lpcvFileData;
+
+	if (!CEterPackManager::Instance().Get(kMappedFile, c_szFileName, &lpcvFileData))
+	{
+		Tracen("Error");
+		TraceError("CTerrainImpl::RAW_LoadTileMap - %s OPEN ERROR", c_szFileName);
+		return false;
+	}
+
+	memcpy(m_abyTileMap, lpcvFileData, sizeof(uint8_t) * (TILEMAP_RAW_XSIZE) * (TILEMAP_RAW_YSIZE));
+
+	return true;
+}
+
+bool CTerrainImpl::LoadWaterMap(const char * c_szFileName)
+{
+	uint32_t dwStart = ELTimer_GetMSec();
+
+	if (!LoadWaterMapFile(c_szFileName))
+	{
+		msl::refill(m_abyWaterMap, 0xFF);
+
+		m_byNumWater = 0;
+		msl::refill(m_lWaterHeight, -1);
+
+		TraceError("CMapOutdoor::LoadWaterMap LoadWaterMapFile(%s) Failed", c_szFileName);
+
+		return false;
+	}
+
+	Tracef("LoadWaterMapFile %s %d\n", c_szFileName, ELTimer_GetMSec() - dwStart);
+	return true;
+}
+
+bool CTerrainImpl::LoadWaterMapFile(const char * c_szFileName)
+{
+	CMappedFile kMappedFile;
+	LPCVOID lpcvFileData;
+
+	if (!CEterPackManager::Instance().Get(kMappedFile, c_szFileName, &lpcvFileData))
+	{
+		Tracen("Error");
+		TraceError("CTerrainImpl::LoadWaterMap - %s OPEN ERROR", c_szFileName);
+		return false;
+	}
+
+	uint32_t dwFileSize = kMappedFile.Size();
+	auto * abFileData = (uint8_t *) lpcvFileData;
+
+	{
+#pragma pack(push)
+#pragma pack(1)
+		struct SWaterMapHeader
+		{
+			uint16_t m_wMagic;
+			uint16_t m_wWidth;
+			uint16_t m_wHeight;
+			uint8_t m_byLayerCount;
+		};
+#pragma pack(pop)
+
+		if (dwFileSize < sizeof(SWaterMapHeader))
+		{
+			TraceError("CTerrainImpl::LoadWaterMap - %s FILE SIZE ERROR", c_szFileName);
+			return false;
+		}
+
+		SWaterMapHeader kWaterMapHeader;
+		memcpy(&kWaterMapHeader, abFileData, sizeof(kWaterMapHeader));
+
+		const uint16_t c_wWaterMapMagic = 5426;
+
+		if (c_wWaterMapMagic != kWaterMapHeader.m_wMagic)
+		{
+			TraceError("CTerrainImpl::LoadWaterMap - %s MAGIC NUMBER(%d!=MAGIC[%d]) ERROR", c_szFileName, kWaterMapHeader.m_wMagic,
+					   c_wWaterMapMagic);
+			return false;
+		}
+
+		if (WATERMAP_XSIZE != kWaterMapHeader.m_wWidth)
+		{
+			TraceError("CTerrainImpl::LoadWaterMap - kWaterMapHeader(%s).m_width(%d)!=WATERMAP_XSIZE(%d)", c_szFileName,
+					   kWaterMapHeader.m_wWidth, WATERMAP_XSIZE);
+			return false;
+		}
+
+		if (WATERMAP_YSIZE != kWaterMapHeader.m_wHeight)
+		{
+			TraceError("CTerrainImpl::LoadWaterMap - kWaterMapHeader(%s).m_height(%d)!=WATERMAP_YSIZE(%d)", c_szFileName,
+					   kWaterMapHeader.m_wHeight, WATERMAP_YSIZE);
+			return false;
+		}
+
+		m_byNumWater = kWaterMapHeader.m_byLayerCount;
+
+		uint32_t dwFileRestSize = dwFileSize - sizeof(kWaterMapHeader);
+		uint32_t dwFileNeedSize = sizeof(m_abyWaterMap) + sizeof(long) * m_byNumWater;
+		uint32_t dwFileNeedSize2 = sizeof(m_abyWaterMap) + sizeof(uint16_t) * m_byNumWater;
+		if (dwFileRestSize == dwFileNeedSize2)
+		{
+			uint16_t wWaterHeight[MAX_WATER_NUM + 1];
+
+			uint8_t * abSrcWaterData = abFileData + sizeof(kWaterMapHeader);
+			memcpy(m_abyWaterMap, abSrcWaterData, sizeof(m_abyWaterMap));
+
+			uint8_t * abSrcWaterHeight = abSrcWaterData + sizeof(m_abyWaterMap);
+
+			m_byNumWater = MIN(MAX_WATER_NUM, m_byNumWater);
+			if (m_byNumWater)
+			{
+				memcpy(wWaterHeight, abSrcWaterHeight, sizeof(uint16_t) * m_byNumWater);
+
+				for (int i = 0; i < m_byNumWater; ++i)
+					m_lWaterHeight[i] = wWaterHeight[i];
+			}
+		}
+		else if (dwFileRestSize != dwFileNeedSize)
+		{
+			TraceError("CTerrainImpl::LoadWaterMap - %s FILE DATA SIZE(rest %d != need %d) ERROR", c_szFileName, dwFileRestSize,
+					   dwFileNeedSize);
+			return false;
+		}
+
+		uint8_t * abSrcWaterData = abFileData + sizeof(kWaterMapHeader);
+		memcpy(m_abyWaterMap, abSrcWaterData, sizeof(m_abyWaterMap));
+
+		uint8_t * abSrcWaterHeight = abSrcWaterData + sizeof(m_abyWaterMap);
+
+		if (m_byNumWater)
+			memcpy(m_lWaterHeight, abSrcWaterHeight, sizeof(long) * m_byNumWater);
+	}
+
+	return true;
+}
+
+uint32_t CTerrainImpl::GetShadowMapColor(float fx, float fy)
+{
+	auto fMapSize = (float) (TERRAIN_XSIZE);
+	float fooMapSize = 1.0f / fMapSize;
+	if (fx < 0 || fy < 0 || fx >= fMapSize || fy >= fMapSize)
+		return 0xFFFFFFFF;
+
+	fx = fx * fooMapSize * (float) (SHADOWMAP_XSIZE - 1);
+	fy = fy * fooMapSize * (float) (SHADOWMAP_YSIZE - 1);
+	int ix, iy;
+	PR_FLOAT_TO_INT(fx, ix);
+	PR_FLOAT_TO_INT(fy, iy);
+
+	uint16_t w = *(m_awShadowMap + (iy * SHADOWMAP_XSIZE) + ix);
+
+	int b = w & 0x1f;
+	w >>= 5;
+	b <<= 3;
+	int g = w & 0x1f;
+	w >>= 5;
+	g <<= 3;
+	int r = w & 0x1f;
+	r <<= 3;
+
+	return (uint32_t)(0xff << 24) | (g << 16) | (g << 8) | r;
+}
---
+++ PRTerrainLib/TextureSet.cpp
@@ -0,0 +1,249 @@
+#include "StdAfx.h"
+#include "TextureSet.h"
+
+CTextureSet::CTextureSet()
+{
+	Initialize();
+}
+
+CTextureSet::~CTextureSet()
+{
+	Clear();
+}
+
+void CTextureSet::Initialize() {}
+
+void CTextureSet::Create()
+{
+	CResource * pResource = CResourceManager::Instance().GetResourcePointer("d:/ymir work/special/error.tga");
+	m_ErrorTexture.ImageInstance.SetImagePointer(msl::inherit_cast<CGraphicImage *>(pResource));
+	AddEmptyTexture(); // 지우개 텍스춰를 처음에 추가 해야 함
+}
+
+bool CTextureSet::Load(const char * c_szTextureSetFileName, float fTerrainTexCoordBase)
+{
+	NANOBEGIN
+	Clear();
+
+	CTokenVectorMap stTokenVectorMap;
+
+	if (!LoadMultipleTextData(c_szTextureSetFileName, stTokenVectorMap))
+	{
+		TraceError("TextureSet::Load : cannot load %s", c_szTextureSetFileName);
+		return false;
+	}
+
+	if (stTokenVectorMap.end() == stTokenVectorMap.find("textureset"))
+	{
+		TraceError("TextureSet::Load : syntax error, TextureSet (filename: %s)", c_szTextureSetFileName);
+		return false;
+	}
+
+	if (stTokenVectorMap.end() == stTokenVectorMap.find("texturecount"))
+	{
+		TraceError("TextureSet::Load : syntax error, TextureCount (filename: %s)", c_szTextureSetFileName);
+		return false;
+	}
+
+	Create();
+
+	const std::string & c_rstrCount = stTokenVectorMap["texturecount"][0];
+
+	long lCount = atol(c_rstrCount.c_str());
+	char szTextureName[32 + 1];
+
+	m_Textures.resize(lCount + 1);
+
+	for (long i = 0; i < lCount; ++i)
+	{
+		_snprintf(szTextureName, sizeof(szTextureName), "texture%03d", i + 1);
+
+		if (stTokenVectorMap.end() == stTokenVectorMap.find(szTextureName))
+			continue;
+
+		const CTokenVector & rVector = stTokenVectorMap[szTextureName];
+
+		const std::string & c_rstrFileName = rVector[0];
+		const std::string & c_rstrUScale = rVector[1];
+		const std::string & c_rstrVScale = rVector[2];
+		const std::string & c_rstrUOffset = rVector[3];
+		const std::string & c_rstrVOffset = rVector[4];
+		const std::string & c_rstrbSplat = rVector[5];
+		const std::string & c_rstrBegin = rVector[6];
+		const std::string & c_rstrEnd = rVector[7];
+
+		float fuScale, fvScale, fuOffset, fvOffset;
+		bool bSplat;
+		uint16_t usBegin, usEnd;
+
+		fuScale = atof(c_rstrUScale.c_str());
+		fvScale = atof(c_rstrVScale.c_str());
+		fuOffset = atof(c_rstrUOffset.c_str());
+		fvOffset = atof(c_rstrVOffset.c_str());
+		bSplat = 0 != atoi(c_rstrbSplat.c_str());
+		usBegin = static_cast<uint16_t>(atoi(c_rstrBegin.c_str()));
+		usEnd = static_cast<uint16_t>(atoi(c_rstrEnd.c_str()));
+
+		if (!SetTexture(i + 1, c_rstrFileName.c_str(), fuScale, fvScale, fuOffset, fvOffset, bSplat, usBegin, usEnd, fTerrainTexCoordBase))
+			TraceError("CTextureSet::Load : SetTexture failed : Filename: %s", c_rstrFileName.c_str());
+	}
+
+	m_stFileName.assign(c_szTextureSetFileName);
+	NANOEND
+	return true;
+}
+
+void CTextureSet::Clear()
+{
+	m_ErrorTexture.ImageInstance.Destroy();
+	m_Textures.clear();
+	Initialize();
+}
+
+void CTextureSet::AddEmptyTexture()
+{
+	TTerrainTexture eraser;
+	m_Textures.emplace_back(eraser);
+}
+
+unsigned long CTextureSet::GetTextureCount()
+{
+	return m_Textures.size();
+}
+
+TTerrainTexture & CTextureSet::GetTexture(unsigned long ulIndex)
+{
+	if (GetTextureCount() <= ulIndex)
+		return m_ErrorTexture;
+
+	return m_Textures[ulIndex];
+}
+
+bool CTextureSet::SetTexture(unsigned long ulIndex, const char * c_szFileName, float fuScale, float fvScale, float fuOffset, float fvOffset,
+							 bool bSplat, uint16_t usBegin, uint16_t usEnd, float fTerrainTexCoordBase)
+{
+	if (ulIndex >= m_Textures.size())
+	{
+		TraceError("CTextureSet::SetTexture : Index Error : Index(%d) is Larger than TextureSet Size(%d)", ulIndex, m_Textures.size());
+		return false;
+	}
+
+	CResource * pResource = CResourceManager::Instance().GetResourcePointer(c_szFileName);
+
+	if (!pResource->IsType(CGraphicImage::Type()))
+	{
+		TraceError("CTerrainImpl::GenerateTexture : %s is NOT Image File", pResource->GetFileName());
+		return false;
+	}
+
+	TTerrainTexture & tex = m_Textures[ulIndex];
+
+	tex.stFilename = c_szFileName;
+	tex.UScale = fuScale;
+	tex.VScale = fvScale;
+	tex.UOffset = fuOffset;
+	tex.VOffset = fvOffset;
+	tex.bSplat = bSplat;
+	tex.Begin = usBegin;
+	tex.End = usEnd;
+	tex.ImageInstance.SetImagePointer(msl::inherit_cast<CGraphicImage *>(pResource));
+	tex.pd3dTexture = tex.ImageInstance.GetTexturePointer()->GetD3DTexture();
+
+
+	D3DXMatrixScaling(&tex.m_matTransform, fTerrainTexCoordBase * tex.UScale, -fTerrainTexCoordBase * tex.VScale, 0.0f);
+	tex.m_matTransform._41 = tex.UOffset;
+	tex.m_matTransform._42 = -tex.VOffset;
+	return true;
+}
+
+void CTextureSet::Reload(float fTerrainTexCoordBase)
+{
+	for (uint32_t dwIndex = 1; dwIndex < GetTextureCount(); ++dwIndex)
+	{
+		TTerrainTexture & tex = m_Textures[dwIndex];
+
+		tex.ImageInstance.ReloadImagePointer(
+			msl::inherit_cast<CGraphicImage *>(CResourceManager::Instance().GetResourcePointer(tex.stFilename.c_str())));
+		tex.pd3dTexture = tex.ImageInstance.GetTexturePointer()->GetD3DTexture();
+
+		D3DXMatrixScaling(&tex.m_matTransform, fTerrainTexCoordBase * tex.UScale, -fTerrainTexCoordBase * tex.VScale, 0.0f);
+		tex.m_matTransform._41 = tex.UOffset;
+		tex.m_matTransform._42 = -tex.VOffset;
+	}
+}
+
+bool CTextureSet::AddTexture(const char * c_szFileName, float fuScale, float fvScale, float fuOffset, float fvOffset, bool bSplat,
+							 uint16_t usBegin, uint16_t usEnd, float fTerrainTexCoordBase)
+{
+	if (GetTextureCount() >= 256)
+	{
+		LogBox("You cannot add more than 255 texture.");
+		return false;
+	}
+
+	for (unsigned long i = 1; i < GetTextureCount(); ++i)
+	{
+		if (c_szFileName == m_Textures[i].stFilename)
+		{
+			LogBox("Texture of the same name already exists.", "Duplicate");
+			return false;
+		}
+	}
+
+	CResource * pResource = CResourceManager::Instance().GetResourcePointer(c_szFileName);
+
+	if (!pResource->IsType(CGraphicImage::Type()))
+	{
+		LogBox("CTerrainImpl::GenerateTexture : It's not an image file. %s", pResource->GetFileName());
+		return false;
+	}
+
+	m_Textures.reserve(m_Textures.size() + 1);
+
+	// @fixme003
+	AddEmptyTexture();
+	SetTexture(m_Textures.size() - 1, c_szFileName, fuScale, fvScale, fuOffset, fvOffset, bSplat, usBegin, usEnd, fTerrainTexCoordBase);
+
+	return true;
+}
+
+bool CTextureSet::RemoveTexture(unsigned long ulIndex)
+{
+	if (GetTextureCount() <= ulIndex)
+		return false;
+
+	auto itor = m_Textures.begin() + ulIndex;
+	m_Textures.erase(itor);
+	return true;
+}
+
+bool CTextureSet::Save(const char * c_pszFileName)
+{
+	msl::file_ptr fPtr(c_pszFileName, "w");
+	if (!fPtr)
+		return false;
+
+	fprintf(fPtr.get(), "TextureSet\n");
+	fprintf(fPtr.get(), "\n");
+
+	// @fixme004
+	fprintf(fPtr.get(), "TextureCount %ld\n", GetTextureCount() ? (GetTextureCount() - 1) : 0);
+	fprintf(fPtr.get(), "\n");
+
+	for (uint32_t i = 1; i < GetTextureCount(); ++i)
+	{
+		TTerrainTexture & rTex = m_Textures[i];
+
+		fprintf(fPtr.get(), "Start Texture%03u\n", i);
+		fprintf(fPtr.get(), "    \"%s\"\n", rTex.stFilename.c_str());
+		fprintf(fPtr.get(), "    %f\n", rTex.UScale);
+		fprintf(fPtr.get(), "    %f\n", rTex.VScale);
+		fprintf(fPtr.get(), "    %f\n", rTex.UOffset);
+		fprintf(fPtr.get(), "    %f\n", rTex.VOffset);
+		fprintf(fPtr.get(), "    %d\n", rTex.bSplat);
+		fprintf(fPtr.get(), "    %hu\n", rTex.Begin);
+		fprintf(fPtr.get(), "    %hu\n", rTex.End);
+		fprintf(fPtr.get(), "End Texture%03u\n", i);
+	}
+	return true;
+}
